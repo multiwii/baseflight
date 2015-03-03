b964c7aa295734ba915e240f7b4de076843429c8
Andreas Tacke <at@mail.fiendie.net>
2015-03-03 09:45:31 +0100

    Now it should work...

--- src/sensors.c
+++ src/sensors.c
@@ -418,7 +418,7 @@ static void GYRO_Common(void)
     static int32_t g[3];
     static stdev_t var[3];
 
-    if (calibratingG > 0) {
+    if(calibratingG > 0){
         for (axis = 0; axis < 3; axis++) {
             // Reset g[axis] at start of calibration
             if (calibratingG == CALIBRATING_GYRO_CYCLES) {
